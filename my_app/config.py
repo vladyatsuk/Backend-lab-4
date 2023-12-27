7fba0eff18738b5631d65b3531db638932af65c5
vladyatsuk <yatsuk.vladislav@gmail.com>
2023-12-27 19:46:11 +0200

    Remove unnecessary things, change DB, add jwt key

--- my_app/config.py
+++ my_app/config.py
@@ -2,9 +2,8 @@ import os
 
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
-# SQLALCHEMY_DATABASE_URI = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_HOST"]}/{os.environ["POSTGRES_DB"]}'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3228@postgres:5432/mydb'
-SQLALCHEMY_DATABASE_URI = 'postgresql://mydb_0nle_user:nHUxDpnvNxEIldD4WLuB6rhpP1BoU3uF@dpg-cm5jjf21hbls73ak45d0-a.oregon-postgres.render.com/mydb_0nle'
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///data.db?check_same_thread=False"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
